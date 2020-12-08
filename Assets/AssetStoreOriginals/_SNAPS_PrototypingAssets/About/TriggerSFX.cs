044289a115171ce2fbba6052f1c001111eaf353a
LaneAckerman <71029213+LaneAckerman@users.noreply.github.com>
2020-12-07 21:17:47 -0500

    midterm
    midterm

---
+++ Assets/AssetStoreOriginals/_SNAPS_PrototypingAssets/About/TriggerSFX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSFX: MonoBehaviour
+{
+    
+
+    public AudioSource playSound;
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        playSound.Play();
+    }
+
+}
