0cbce2ab07907d2e14ce6bef11526bf772b7325e
LaneAckerman <71029213+LaneAckerman@users.noreply.github.com>
2020-12-13 19:42:14 -0500

    Final Project
    Final project submission

---
+++ Assets/CoffinTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffinTrigger : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+         animator = GetComponentInChildren<Animator>();
+
+    }
+    
+    public void OnTriggerEnter(Collider collider)
+    {
+        animator.SetTrigger("CoffinTrigger");
+    }
+
+
+}
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public string Main;
+	public string Secondary;
+	public string Menu;
+
+
+
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene(Main);
+
+	}
+
+	public void  SecondaryScene ()
+	{
+		SceneManager.LoadScene(Secondary);
+
+	}
+
+	public void ExitToMenu ()
+	{
+		SceneManager.LoadScene(Menu);
+	}
+
+	public void QuitGame ()
+	{
+		Debug.Log("Quit!");
+
+		Application.Quit();
+	}
+}
